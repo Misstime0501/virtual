0327d94191136a739877e8cc1f4756d52ac45d75
Misstime0501 <wolichen0501@gmail.com>
2015-04-15 11:41:37 +0800

    创建一个空项目

---
+++ virtual_phone/virtual_phone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  virtual_phone
+//
+//  Created by HRWY on 15/4/15.
+//  Copyright (c) 2015年 HRWY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ virtual_phone/virtual_phone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  virtual_phone
+//
+//  Created by HRWY on 15/4/15.
+//  Copyright (c) 2015年 HRWY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
